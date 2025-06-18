b113e52b762dc8a480b46ab3587e8aeeec1dbb55
Hiperultimate <hiperultimatevegito@gmail.com>
2025-06-18 13:20:12 +0530

    Adding token smartcontract and another contract to act as a wormhole token that contract

--- script/HampToken.s.sol
+++ script/HampToken.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {HampToken} from "../src/HampToken.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract HampTokenScript is Script {
+    HampToken public token;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        token = new HampToken();
 
         vm.stopBroadcast();
     }
---
+++ src/HampToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract HampToken is ERC20, Ownable { 
+
+    constructor(uint _initSupply) ERC20("Hamp", "HMP") Ownable(msg.sender){
+
+    }
+
+    function mint(address account , uint amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+}
